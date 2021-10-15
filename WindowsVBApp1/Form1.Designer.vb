d945cae9c9c0b6e430938234375ed9e03749b4b5
Kento Shirasawa <shirasawa.kento@gmail.com>
2021-10-16 00:02:40 +0900

    first commit

---
+++ WindowsVBApp1/Form1.Designer.vb
@@ -0,0 +1,244 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class MainForm
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.MainTextBox = New System.Windows.Forms.TextBox()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.編集ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.表示ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ツールToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.FormatToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.FontToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ヘルプToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
+        Me.FontSelectorDialog = New System.Windows.Forms.FontDialog()
+        Me.NewToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.RTLToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.開くToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.保存ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.バージョン情報ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripStatusLabel_CharCode = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.UndoToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.RedoToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripStatusLabel_Line = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.ToolStripStatusLabel_Col = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.MenuStrip1.SuspendLayout()
+        Me.StatusStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MainTextBox
+        '
+        Me.MainTextBox.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.MainTextBox.Font = New System.Drawing.Font("MS UI Gothic", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.MainTextBox.Location = New System.Drawing.Point(0, 27)
+        Me.MainTextBox.Multiline = True
+        Me.MainTextBox.Name = "MainTextBox"
+        Me.MainTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.MainTextBox.Size = New System.Drawing.Size(1124, 561)
+        Me.MainTextBox.TabIndex = 0
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem, Me.編集ToolStripMenuItem, Me.表示ToolStripMenuItem, Me.ツールToolStripMenuItem, Me.FormatToolStripMenuItem, Me.ヘルプToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(1124, 24)
+        Me.MenuStrip1.TabIndex = 1
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.NewToolStripMenuItem, Me.開くToolStripMenuItem, Me.保存ToolStripMenuItem, Me.ExitToolStripMenuItem})
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.ShortcutKeyDisplayString = ""
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(56, 20)
+        Me.FileToolStripMenuItem.Text = "ファイル"
+        '
+        '編集ToolStripMenuItem
+        '
+        Me.編集ToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.UndoToolStripMenuItem, Me.RedoToolStripMenuItem})
+        Me.編集ToolStripMenuItem.Name = "編集ToolStripMenuItem"
+        Me.編集ToolStripMenuItem.Size = New System.Drawing.Size(45, 20)
+        Me.編集ToolStripMenuItem.Text = "編集"
+        '
+        '表示ToolStripMenuItem
+        '
+        Me.表示ToolStripMenuItem.Name = "表示ToolStripMenuItem"
+        Me.表示ToolStripMenuItem.Size = New System.Drawing.Size(45, 20)
+        Me.表示ToolStripMenuItem.Text = "表示"
+        '
+        'ツールToolStripMenuItem
+        '
+        Me.ツールToolStripMenuItem.Name = "ツールToolStripMenuItem"
+        Me.ツールToolStripMenuItem.Size = New System.Drawing.Size(51, 20)
+        Me.ツールToolStripMenuItem.Text = "ツール"
+        '
+        'FormatToolStripMenuItem
+        '
+        Me.FormatToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FontToolStripMenuItem, Me.RTLToolStripMenuItem})
+        Me.FormatToolStripMenuItem.Name = "FormatToolStripMenuItem"
+        Me.FormatToolStripMenuItem.Size = New System.Drawing.Size(75, 20)
+        Me.FormatToolStripMenuItem.Text = "フォーマット"
+        '
+        'FontToolStripMenuItem
+        '
+        Me.FontToolStripMenuItem.Name = "FontToolStripMenuItem"
+        Me.FontToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.FontToolStripMenuItem.Text = "フォント..."
+        '
+        'ヘルプToolStripMenuItem
+        '
+        Me.ヘルプToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.バージョン情報ToolStripMenuItem})
+        Me.ヘルプToolStripMenuItem.Name = "ヘルプToolStripMenuItem"
+        Me.ヘルプToolStripMenuItem.Size = New System.Drawing.Size(51, 20)
+        Me.ヘルプToolStripMenuItem.Text = "ヘルプ"
+        '
+        'StatusStrip1
+        '
+        Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripStatusLabel_CharCode, Me.ToolStripStatusLabel_Line, Me.ToolStripStatusLabel_Col})
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 591)
+        Me.StatusStrip1.Name = "StatusStrip1"
+        Me.StatusStrip1.Size = New System.Drawing.Size(1124, 22)
+        Me.StatusStrip1.TabIndex = 2
+        Me.StatusStrip1.Text = "StatusStrip1"
+        '
+        'NewToolStripMenuItem
+        '
+        Me.NewToolStripMenuItem.Name = "NewToolStripMenuItem"
+        Me.NewToolStripMenuItem.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.N), System.Windows.Forms.Keys)
+        Me.NewToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.NewToolStripMenuItem.Text = "新規作成"
+        '
+        'RTLToolStripMenuItem
+        '
+        Me.RTLToolStripMenuItem.Name = "RTLToolStripMenuItem"
+        Me.RTLToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.RTLToolStripMenuItem.Text = "RTL"
+        '
+        '開くToolStripMenuItem
+        '
+        Me.開くToolStripMenuItem.Name = "開くToolStripMenuItem"
+        Me.開くToolStripMenuItem.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.O), System.Windows.Forms.Keys)
+        Me.開くToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.開くToolStripMenuItem.Text = "開く"
+        '
+        '保存ToolStripMenuItem
+        '
+        Me.保存ToolStripMenuItem.Name = "保存ToolStripMenuItem"
+        Me.保存ToolStripMenuItem.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.S), System.Windows.Forms.Keys)
+        Me.保存ToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.保存ToolStripMenuItem.Text = "保存"
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.ShortcutKeys = CType((System.Windows.Forms.Keys.Alt Or System.Windows.Forms.Keys.F4), System.Windows.Forms.Keys)
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.ExitToolStripMenuItem.Text = "終了"
+        '
+        'バージョン情報ToolStripMenuItem
+        '
+        Me.バージョン情報ToolStripMenuItem.Name = "バージョン情報ToolStripMenuItem"
+        Me.バージョン情報ToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.バージョン情報ToolStripMenuItem.Text = "バージョン情報"
+        '
+        'ToolStripStatusLabel_CharCode
+        '
+        Me.ToolStripStatusLabel_CharCode.Name = "ToolStripStatusLabel_CharCode"
+        Me.ToolStripStatusLabel_CharCode.Size = New System.Drawing.Size(62, 17)
+        Me.ToolStripStatusLabel_CharCode.Text = "文字コード"
+        '
+        'UndoToolStripMenuItem
+        '
+        Me.UndoToolStripMenuItem.Name = "UndoToolStripMenuItem"
+        Me.UndoToolStripMenuItem.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.Z), System.Windows.Forms.Keys)
+        Me.UndoToolStripMenuItem.Size = New System.Drawing.Size(188, 22)
+        Me.UndoToolStripMenuItem.Text = "元に戻す"
+        '
+        'RedoToolStripMenuItem
+        '
+        Me.RedoToolStripMenuItem.Name = "RedoToolStripMenuItem"
+        Me.RedoToolStripMenuItem.ShortcutKeys = CType(((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.Shift) _
+            Or System.Windows.Forms.Keys.Z), System.Windows.Forms.Keys)
+        Me.RedoToolStripMenuItem.Size = New System.Drawing.Size(188, 22)
+        Me.RedoToolStripMenuItem.Text = "やり直し"
+        '
+        'ToolStripStatusLabel_Line
+        '
+        Me.ToolStripStatusLabel_Line.Name = "ToolStripStatusLabel_Line"
+        Me.ToolStripStatusLabel_Line.Size = New System.Drawing.Size(20, 17)
+        Me.ToolStripStatusLabel_Line.Text = "行"
+        '
+        'ToolStripStatusLabel_Col
+        '
+        Me.ToolStripStatusLabel_Col.Name = "ToolStripStatusLabel_Col"
+        Me.ToolStripStatusLabel_Col.Size = New System.Drawing.Size(20, 17)
+        Me.ToolStripStatusLabel_Col.Text = "列"
+        '
+        'MainForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1124, 613)
+        Me.Controls.Add(Me.StatusStrip1)
+        Me.Controls.Add(Me.MainTextBox)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "MainForm"
+        Me.Text = "Form1"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.StatusStrip1.ResumeLayout(False)
+        Me.StatusStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents MainTextBox As TextBox
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents StatusStrip1 As StatusStrip
+    Friend WithEvents FileToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents 編集ToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents 表示ToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ツールToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ヘルプToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents FormatToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents FontToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents FontSelectorDialog As FontDialog
+    Friend WithEvents NewToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents RTLToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents 開くToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents 保存ToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ExitToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents バージョン情報ToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ToolStripStatusLabel_CharCode As ToolStripStatusLabel
+    Friend WithEvents UndoToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents RedoToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ToolStripStatusLabel_Line As ToolStripStatusLabel
+    Friend WithEvents ToolStripStatusLabel_Col As ToolStripStatusLabel
+End Class
---
+++ WindowsVBApp1/Form1.vb
@@ -0,0 +1,92 @@
+﻿Public Class MainForm
+
+    ' ----------
+    ' フォント選択ダイアログ表示
+    ' ----------
+    Private Sub FontToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles FontToolStripMenuItem.Click
+
+        ' フォント選択ダイアログの返り値
+        Dim ret As DialogResult
+
+        Try
+            Using Dialog As New FontDialog()
+                With Dialog
+                    .Font = MainTextBox.Font
+                End With
+
+                ret = Dialog.ShowDialog()
+
+                If ret = DialogResult.OK Then
+                    MainTextBox.Font = Dialog.Font
+                End If
+            End Using
+        Catch ex As Exception
+            Debug.WriteLine(ex)
+        End Try
+
+    End Sub
+
+    ' ----------
+    ' ウィンドウを閉じる際の処理
+    ' ----------
+    Private Sub MainForm_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
+
+        If MainTextBox.Text IsNot "" Then
+            MsgBox("終了します。よろしいですか？")
+        End If
+
+    End Sub
+
+    ' ----------
+    ' RTL切り替え
+    ' ----------
+    Private Sub RTLToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles RTLToolStripMenuItem.Click
+        If MainTextBox.RightToLeft = RightToLeft.No Then
+            MainTextBox.RightToLeft = RightToLeft.Yes
+            RTLToolStripMenuItem.Checked = True
+        Else
+            MainTextBox.RightToLeft = RightToLeft.No
+            RTLToolStripMenuItem.Checked = False
+        End If
+    End Sub
+
+    ' ----------
+    ' フォームロード時
+    ' ----------
+    Private Sub MainForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ToolStripStatusLabel_Line.Text = "行: " + MainTextBox.SelectionStart.ToString
+    End Sub
+
+    ' ----------
+    ' 元に戻す（アンドゥ）
+    ' ----------
+    Private Sub UndoToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles UndoToolStripMenuItem.Click
+        If MainTextBox.CanUndo = True Then
+            MainTextBox.Undo()
+        End If
+    End Sub
+
+    ' ----------
+    ' やり直し（リドゥ）
+    ' ----------
+    Private Sub RedoToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles RedoToolStripMenuItem.Click
+
+    End Sub
+
+    ' ----------
+    ' アプリケーション終了
+    ' ----------
+    Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
+        Application.Exit()
+    End Sub
+
+    ' ----------
+    ' 新規作成
+    ' ----------
+    Private Sub NewToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles NewToolStripMenuItem.Click
+        If MainTextBox.Text IsNot "" Then
+            MsgBox("編集した内容は失われます。")
+        End If
+        MainTextBox.Text = ""
+    End Sub
+End Class
